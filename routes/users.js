7efb16ea2d41810b678f1645d5250d79acdc3258
TURAROVICH <turaaa555@gmail.com>
2022-01-17 15:22:17 +0600

    test8

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,7 @@ const authonticateToken = require('../services/checkAuth')
 
 router.get('/users', authonticateToken ,async (req, res) => {
     const users = await Users.find({})
-    const accessToken = req.accessToken
-    res.json({users,accessToken})
+    res.json({users})
 })
 
 
--- services/checkAuth.js
+++ services/checkAuth.js
@@ -9,9 +9,10 @@ async function authonticateToken(req,res,next){
         console.log(req.cookies.REFRESH_TOKEN);
         const refresh = req.cookies.REFRESH_TOKEN 
         const user =refresh && await Users.findOne({refreshToken:refresh})
+        console.log(user);
         if(!user) return res.sendStatus(403)
         const accessToken = jwt.sign({password:user.password,email:user.email},ACCESS_TOKEN,{expiresIn: accesTokenTime + 's'})
-        req.accessToken = accessToken
+        res.json({accessToken})
         next()
     }
     jwt.verify(token,ACCESS_TOKEN,(err,user)=>{
